38355982098b648d196454c57de0cd1ed024a8a2
BharathMood <bmood01@slu.edu>
2025-02-11 23:43:14 +0000

    lab 4 changes

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const fs = require('fs').promises
 const path = require('path')
 const express = require('express')
+const api = require('./api')
+const middleware = require('./middleware')
 
 // Set the port
 const port = process.env.PORT || 3000
-// Boot the app
-const app = express()
+@@ -9,31 +11,11 @@ const app = express()
 // Register the public directory
 app.use(express.static(__dirname + '/public'));
 // register the routes
 app.get('/products', listProducts)
 app.get('/', handleRoot);
+app.use(middleware.cors)
+app.get('/', api.handleRoot)
+app.get('/products', api.listProducts)
+app.get('/products/:id', api.getProduct)
+app.delete('/products/:id', api.deleteProduct);
+app.put('/products/:id', api.updateProduct);
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
